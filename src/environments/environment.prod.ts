d8a1106e24d71bf5ca216978739ac13723a15d8e
Olivier Mutombo (Group) <olivier.mutombo@dimensiondata.com>
2020-08-13 08:51:54 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8080',
+  apiVersion: '/api/v1',
+  baseUrl: 'http://localhost:8080/api/v1',
+  apiUsername: 'payroll-client',
+  apiPassword: 'payroll-secret'
 };
