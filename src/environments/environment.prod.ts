e238dd7ae41c0137ca078cf36e3d7062c563082d
Olivier Mutombo (Group) <olivier.mutombo@dimensiondata.com>
2020-08-13 09:53:15 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://payrollsystem-payroll-system.apps.us-east-2.starter.openshift-online.com/oauth/token',
   apiVersion: '/api/v1',
-  baseUrl: 'http://localhost:8080/api/v1',
+  baseUrl: 'http://payrollsystem-payroll-system.apps.us-east-2.starter.openshift-online.com/oauth/token/api/v1',
   apiUsername: 'payroll-client',
   apiPassword: 'payroll-secret'
 };
